1e672871ba54b2a334b31563cfaeb405b221840a
bszyca <szycabarbara@gmail.com>
2026-03-06 19:20:51 +0100

    Initial commit of rc_car project

---
+++ app/car/main.c
@@ -0,0 +1,12 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+int main(void)
+{
+    while (1) {
+        printk("RC car alive\n");
+        k_sleep(K_SECONDS(1));
+    }
+
+    return 0;
+}
